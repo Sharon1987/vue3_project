236248347c0738f3ad3af69d69a5cf5082b891dd
Sharon1987 <scwu.ie99g@gmail.com>
2022-02-06 02:19:29 +0800

    week4 分頁元件、Modal元件

--- js/productList.js
+++ js/productList.js
@@ -2,17 +2,17 @@ import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.
 let productModal = null;
 let delProductModal = null;
 
-createApp({
+const app =createApp({
   data() {
     return {
       apiUrl: 'https://vue3-course-api.hexschool.io/v2',
       apiPath: 'sharon1987',
       products: [],
-      isNew:false, //是否是新增的商品
-      
+      isNew: false,
       tempProduct: {
-        imagesUrl:[],
+        imagesUrl: [],
       },
+      pagination: {},
     }
   },
   methods: {
@@ -24,100 +24,138 @@ createApp({
         })
         .catch((err) => {
           alert(err.data.message)
+          window.location = 'index.html';
+        })
+    },
+    getData(page = 1) {
+      const url = `${this.apiUrl}/api/${this.apiPath}/admin/products?page=${page}`;
+
+      axios.get(url)
+        .then((response) => {
+          const { products, pagination } = response.data;
+          this.products = products;
+          this.pagination = pagination;
+        }).catch((err) => {
+          alert(err.data.message);
           window.location = 'login.html';
         })
     },
-    getData() {
-      const url = `${this.apiUrl}/api/${this.apiPath}/admin/products/all`;
-      axios.get(url)
-        .then((response) => {
-          this.products = response.data.products;
-        })
-        .catch((err) => {
-          alert(err.data.message);
-        })
+    openModal(modalStatus, item) {
+      if (modalStatus === 'new') {
+        this.tempProduct = {
+          imagesUrl: [],
+        };
+        this.isNew = true;
+        productModal.show();
+      } else if (modalStatus === 'edit') {
+        this.tempProduct = { ...item };
+        this.isNew = false;
+        productModal.show();
+      } else if (modalStatus === 'delete') {
+        this.tempProduct = { ...item };
+        delProductModal.show()
+      }
     },
-    updateProduct() {
-    //更新商品列表
-    let url= `${this.apiUrl}/api/${this.apiPath}/admin/product`;
-    let http='';
-
-    if(this.isNew===""){
-      //isNew狀態為""時,打刪除API
-      url+= `/${this.tempProduct.id}`;
-      http='delete';
-      axios[http](url).then((response) => {
-        alert(response.data.message);
-        delProductModal.hide();
-        this.getData();
-      }).catch((err) => {
-        alert(err.data.message);
-      })
-      
-    }    
- 
-else{ 
-//isNew狀態不為""的時候,判斷是新增商品或是編輯商品
-if(this.isNew){
-  //新增商品
-http='post';
-}
-else if(!this.isNew){
-//不是新增商品(編輯商品),編輯商品要帶入product.id
-http='put';
-url+= `/${this.tempProduct.id}`;
-}
-axios[http](url, { data: this.tempProduct }).then((res) => {
-  alert(res.data.message);
-  productModal.hide();
-  this.getData();
-}).catch((err) => {
-  alert(err.data.message);
-})
-}
   },
-
-  openModal(modalStatus,item){
- if(modalStatus==="new"){
-   this.tempProduct={
-imagesUrl:[],
-   };
-   this.isNew=true;
-   productModal.show();
- }else if(modalStatus==="edit"){
-   this.tempProduct={...item};
-   this.isNew=false;
-   productModal.show();
- }else if(modalStatus==="delete"){
-   this.tempProduct={...item};
-   this.isNew="";
-   delProductModal.show();
- }
-  },
-
-  createImages() {
-    this.tempProduct.imagesUrl = [];
-    this.tempProduct.imagesUrl.push('');
-  },
-
-}, 
   mounted() {
-
-    productModal = new bootstrap.Modal(document.getElementById('productModal'), {
-      keyboard: false
-    });
-
-    delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
-      keyboard: false
-    });
-
-    // 取出 Token
-    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
     axios.defaults.headers.common.Authorization = token;
-    this.checkAdmin()
-  }
-}).mount('#app');
+    this.checkAdmin();
+  },
+});
 
+// 建立分頁元件
+app.component('pagination', {
+  template: '#pagination',
+  props: ['pages'],
+  methods: {
+    emitPages(item) {
+      this.$emit('emit-pages', item);
+    },
+  },
+});
 
+// 建立新增/編輯產品的元件Modal
+app.component('productModal', {
+  template: '#productModal',
+  props: ['product', 'isNew'],
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      apiPath: 'sharon1987',
+      modal: null,
+    };
+  },
+  mounted() {
+    productModal = new bootstrap.Modal(document.getElementById('productModal'), {
+      keyboard: false,
+      backdrop: 'static'
+    });
+  },
+  methods: {
+    updateProduct() {
+      // 預設為新增商品
+      let api = `${this.apiUrl}/api/${this.apiPath}/admin/product`;
+      let http = 'post';
+      // 如果不是新增商品的話切改成打編輯商品的API
+      if (!this.isNew) {
+        api += `/${this.product.id}`;
+        http = 'put';
+      }
 
+      axios[http](api, { data: this.product }).then((response) => {
+        alert(response.data.message);
+        this.hideModal();
+        this.$emit('update');
+      }).catch((error) => {
+        alert(error.data.message);
+      });
+    },
+    createImages() {
+      this.product.imagesUrl = [];
+      this.product.imagesUrl.push('');
+    },
+    openModal() {
+      productModal.show();
+    },
+    hideModal() {
+      productModal.hide();
+    },
+  },
+})
+// 產品刪除元件Modal視窗
+app.component('delProductModal', {
+  template: '#delProductModal',
+  props: ['item'],
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      apiPath: 'sharon1987',
+      modal: null,
+    };
+  },
+  mounted() {
+    delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
+      keyboard: false,
+      backdrop: 'static',
+    });
+  },
+  methods: {
+    delProduct() {
+      axios.delete(`${this.apiUrl}/api/${this.apiPath}/admin/product/${this.item.id}`).then((response) => {
+        this.hideModal();
+        this.$emit('update');
+      }).catch((error) => {
+        alert(error.data.message);
+      });
+    },
+    openModal() {
+      delProductModal.show();
+    },
+    hideModal() {
+      delProductModal.hide();
+    },
+  },
+});
 
+app.mount('#app');
